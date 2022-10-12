ba7cbd231a2be7eb99e2f34517f0912aafd902b4
atiyeh <mahboob6103@gmail.com>
2022-10-12 22:23:07 +0330

    feat:make icons for folderstucture

---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,18 @@
+import { BiShoppingBag } from "react-icons/bi";
+import {FaRegUser} from 'react-icons/fa'
+import {GiHamburgerMenu} from 'react-icons/gi'
+import {MdClose} from 'react-icons/md'
+interface icons{
+  BiShoppingBag:any;
+  FaRegUser:any;
+  GiHamburgerMenu:any;
+  MdClose:any;
+  
+}
+const allIcons:icons = {
+  BiShoppingBag,
+  FaRegUser,
+  GiHamburgerMenu,
+  MdClose,
+};
+export default allIcons;
