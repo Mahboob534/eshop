7f876509dd9812e0da65560db5d192a1e50f8d4e
atiyeh <mahboob6103@gmail.com>
2022-10-15 16:38:14 +0330

    feat:add src folder to folder folder structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from "react";
+import Header from "./layout/header/Header";
+import Footer from "./layout/footer/Footer";
+import LayoutUser from "./layout/LayoutUser";
+
+function App() {
+  return (
+    <>
+      <LayoutUser />
+    </>
+  );
+}
+
+export default App;
