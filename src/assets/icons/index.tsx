8c5dbe5b2dd75859f6661e7a3cb97cd804b703e6
atiyeh <mahboob6103@gmail.com>
2022-10-15 19:50:15 +0330

    feat:add icons for menu

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -1,12 +1,24 @@
-import { BiShoppingBag } from "react-icons/bi";
-import {FaRegUser} from 'react-icons/fa'
+import { BiShoppingBag ,BiHeadphone } from "react-icons/bi";
+import {FaRegUser , FaMobileAlt , FaTabletAlt} from 'react-icons/fa'
 import {GiHamburgerMenu} from 'react-icons/gi'
-import {MdClose} from 'react-icons/md'
+import {MdClose ,MdCable} from 'react-icons/md'
+import {BsLaptop} from 'react-icons/bs'
+import {TbDeviceWatchStats} from 'react-icons/tb'
+import {IoGameControllerOutline,IoRadioOutline} from 'react-icons/io5'
+
 interface icons{
   BiShoppingBag:any;
   FaRegUser:any;
   GiHamburgerMenu:any;
   MdClose:any;
+  FaMobileAlt:any;
+  BsLaptop:any;
+  FaTabletAlt:any;
+  TbDeviceWatchStats:any;
+  MdCable:any;
+  IoGameControllerOutline:any;
+  IoRadioOutline:any;
+  BiHeadphone:any;
   
 }
 const allIcons:icons = {
@@ -14,5 +26,14 @@ const allIcons:icons = {
   FaRegUser,
   GiHamburgerMenu,
   MdClose,
+  FaMobileAlt,
+  BsLaptop,
+  FaTabletAlt,
+  TbDeviceWatchStats,
+  MdCable,
+  IoGameControllerOutline,
+  IoRadioOutline,
+  BiHeadphone,
+
 };
 export default allIcons;
