307c234d817d821f9c28adfc069d8116d510518d
atiyeh <mahboob6103@gmail.com>
2022-10-16 20:32:05 +0330

    feat: write menuHorizontal function in men component

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,20 +1,39 @@
 import React, { FC } from "react";
-import { menuItems, lists } from "./MenuItem";
-import allIcons from "../../assets/icons";
-import { Link } from "react-router-dom"
-interface MenuProps {
-  active: boolean;
-  showMenu(): void;
-}
+import { menuItems,lists } from "./MenuItem";
+import Images from '../../assets/Index'
+import { Link } from "react-router-dom";
 
-export default function Menu<FC>(props: MenuProps) {
-  const active = props.active;
-  const showMenu = props.showMenu;
+export  function Menu<FC>() {
   return (
-    <ul>
-     {Object.values(menuItems).map((item: lists,id:number) => (
-        <li key={id} className="border-t border-slate-50 opacity-100 p-2 "><Link to="./"> {item}</Link></li>
+    
+    <ul >
+       <li className="w-full">
+    <img className="w-full h-28" src={Images.logo} alt="logo"/>
+    
+  </li>
+     
+      {menuItems.map((item:lists) => (
+        
+        <li className="w-full border-t border-slate-50 opacity-100 py-2 ">
+          <Link  className="p-2" key={item.id} to="./">{item.title}</Link> 
+        </li>
+        
       ))}
     </ul>
   );
+}
+export const  MenuHorizontal=()=>{
+  return (
+  <ul className="flex flex-wrap text-slate-50">
+      {menuItems.map((item:lists) => (
+         <Link  key={item.id} to="./">
+        <li className=" flex flex-wrap item-center px-5">
+          <span className="text-xl ml-2">{<item.icon/>} </span> 
+         {item.title}
+        </li>
+        </Link>
+      ))}
+    </ul>
+  )
+
 }
