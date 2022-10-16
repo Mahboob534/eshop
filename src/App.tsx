fa7d8c8688d9cbf141c08f90af3eb96a66365975
atiyeh <mahboob6103@gmail.com>
2022-10-16 21:53:10 +0330

    fix:edit app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from "react";
-import Header from "./layout/header/Header";
-import Footer from "./layout/footer/Footer";
+
 import LayoutUser from "./layout/LayoutUser";
 
 function App() {
