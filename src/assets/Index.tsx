616cbe5966405339839d2d55106087c135a06f56
atiyeh <mahboob6103@gmail.com>
2022-10-18 23:04:46 +0330

    fix:add new export image to index

--- src/assets/Index.tsx
+++ src/assets/Index.tsx
@@ -1,9 +1,23 @@
 import logo from "./images/namad.png"
+import tandisHoghogh from './images/تصویر-تندیس-حمایت-از-حقوق-مصرف-کنندگان-فروشگاه-اینترنتی-لیپک-1.png'
+import tandisFanavar from './images/تصویر-تندیس-مدیریت-فناوری-فروشگاه-اینترنتی-لیپک.png'
+import symbolNational from './images/نشان-ملی-ثبت-رسانه-های-ریجیتال-سایت-لیپک.png'
+import symbolElectronic from './images/نماد-الکترونیکی-فروشگاه-انلاین-لیپک.png'
 interface images {
-    logo:any
+    logo:any;
+    tandisHoghogh:any;
+    tandisFanavar:any;
+    symbolNational:any;
+    symbolElectronic:any;
 }
 const Images:images={
     logo,
+    tandisHoghogh,
+    tandisFanavar,
+    symbolNational,
+    symbolElectronic,
+
+
 
 }
 export default Images
