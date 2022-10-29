b0fe060a434af9ab110aa1f23d1425aca342fc22
atiyeh <mahboob6103@gmail.com>
2022-10-29 07:24:07 +0330

    feat:make Display password hook

--- src/hook/DisplayPassword.tsx
+++ src/hook/DisplayPassword.tsx
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
-export const DisplayIcon = (props) => {
- let iconEye= props.iconEye
- let setIconEye= props.setIconEye
- let setPasswordVisibility= props.setPasswordVisibility
- let passwordVisibility= props.passwordVisibility
- 
+import React, {FC, useState } from "react";
+// interface propsPassword{
+//   iconEye:string;
+//   setIconEye(iconEye:string):void;
+//   setPasswordVisibility(passwordVisibility:boolean):void;
+//   passwordVisibility:boolean;
+
+// }
+
+ const DisplayPassword  = () => {
+  const [passwordVisibility, setPasswordVisibility] = useState<boolean>(true);
+  const [iconEye, setIconEye] = useState<string>("eye");
+ const handlePasswordVisibility=()=>{
   if (iconEye === "eye") {
     setIconEye("eye-off");
     setPasswordVisibility(!passwordVisibility);
@@ -12,8 +18,11 @@ export const DisplayIcon = (props) => {
     setIconEye("eye");
     setPasswordVisibility(!passwordVisibility);
   }
-  return({iconEye,passwordVisibility})
+ }
+  
+  return({iconEye,passwordVisibility,handlePasswordVisibility})
    
 
   
 }
+export default DisplayPassword
