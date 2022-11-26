7b142ac34731cbb601d79c708dae2a1f0bfe8336
atiyeh <mahboob6103@gmail.com>
2022-11-26 18:39:19 +0330

    fix: layoutout user component

--- src/layout/LayoutUser.tsx
+++ src/layout/LayoutUser.tsx
@@ -1,29 +1,20 @@
 import React,{FC} from 'react'
-<<<<<<< HEAD
+
 import FooterUser from './footer/FooterUser'
 import HeaderUser from './header/HeaderUser'
 interface propChildren{
   children:JSX.Element;
 }
-
-=======
-
-import Footer from './footer/FooterUser'
-import Header from './header/HeaderUser'
-interface propChildren{
-  children:JSX.Element;
-}
->>>>>>> loginpage
 const LayoutUser: FC<propChildren>= ({children}) =>{
   return(
   <div>
     <div >
         <HeaderUser/>
     </div>
-    <div>
+    <div className='relative  shadow-2xl top-36'>
         {children}
     </div>
-    <div>
+    <div className=' relative  shadow-2xl top-36 '>
         <FooterUser/>
     </div>
 
