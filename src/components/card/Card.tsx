ce1581b1382d901b0eacddd4208839ade9ed9fa4
atiyeh <mahboob6103@gmail.com>
2022-12-07 19:05:06 +0330

    fix:edit card and add information

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,28 +1,36 @@
 import React from "react";
+
+import allIcons from "../../assets/icons/index";
+
+import {toFarsiNumber,numberDivider} from '../../utils/FunctionUtils'
 interface propsProducts {
   product: any;
 }
 export default function Card(props: propsProducts) {
   const products = props.product;
   return (
-    <div className="w-72 h-96 border-1 border-slate-400 rounded  bg-slate-500 shadow-2xl">
-      <div className="flex flex-col justify-center items-center p-3">
-        <div className="w-28 h-32 ">
-          <img className="" />
-        </div>
-        <div className="text-md text-slate-900 font-bold">
-          <p ></p>
-        </div>
-        <div className="flex justify-around items-center ">
-          <div className=" w-16 text-md font-bold text-slate-900 p-2">
-            <p></p>
-
-          </div>
-          <div className="w-16 p-2"></div>
-        </div>
+    <div className=" flex flex-col  p-3 w-72 h-96 border-1 bg-white  rounded-xl shadow-2xl">
+      <div className="w-full  ">
+        <img
+          className="h-72"
+          src={`http://localhost:3002/files/${products.images[0]}`}
+          alt="thumbnail"
+        />
       </div>
 
-      {products.product_name_fa}
+      <div className="w-full  text-md text-slate-900 font-bold  ">
+        {products.product_name_fa}
+      </div>
+      <div className="flex flex-row justify-between items-center font-bold w-full ">
+        <div className="w-16 " >
+          <button type="button" className="text-3xl text-blue-800 hover:text-blue-600" >
+          <allIcons.BiShoppingBag/>
+          </button>
+        </div>
+        <div className="">
+          <p>  <span className="ml-2">قیمت :</span>{numberDivider(toFarsiNumber(products.price.amount))}</p>
+        </div>
+      </div>
     </div>
   );
 }
