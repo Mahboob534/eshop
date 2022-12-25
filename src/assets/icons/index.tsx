b3d7b1a35e3fd9fe8a3278daa5a6b7c56d3eac9c
atiyeh <mahboob6103@gmail.com>
2022-12-25 11:26:54 +0330

    feat:add icons

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -1,55 +1,91 @@
-import { BiShoppingBag ,BiHeadphone } from "react-icons/bi";
-import {FaRegUser , FaMobileAlt , FaTabletAlt,FaLinkedin,FaFacebookF,FaRegEye,FaRegEyeSlash} from 'react-icons/fa'
+import { BiShoppingBag, BiHeadphone } from "react-icons/bi";
+import {
+  FaRegUser,
+  FaMobileAlt,
+  FaTabletAlt,
+  FaLinkedin,
+  FaFacebookF,
+  FaRegEye,
+  FaRegEyeSlash,
+} from "react-icons/fa";
 
-import {MdClose ,MdCable,MdEmail,MdWatchLater,MdChevronRight,MdChevronLeft} from 'react-icons/md'
-import {BsLaptop,BsTelephoneFill,BsTwitter,BsPinterest} from 'react-icons/bs'
-import {TbDeviceWatchStats,TbMessageCircle,TbDiscount2,TbTruckDelivery} from 'react-icons/tb'
-import {IoGameControllerOutline,IoRadioOutline} from 'react-icons/io5'
-import {ImLocation} from 'react-icons/im'
-import {AiFillInstagram , AiOutlineAppstore,AiOutlineArrowLeft,AiOutlineArrowRight} from 'react-icons/ai'
-import {RiCustomerService2Fill} from 'react-icons/ri'
-import {SiAuth0} from 'react-icons/si'
-import {FcApproval} from 'react-icons/fc'
+import {
+  MdClose,
+  MdCable,
+  MdEmail,
+  MdWatchLater,
+  MdChevronRight,
+  MdChevronLeft,
+  MdFavoriteBorder,
+  MdOutlineNotificationsActive,
+  MdCompare
+} from "react-icons/md";
+import {
+  BsLaptop,
+  BsTelephoneFill,
+  BsTwitter,
+  BsPinterest,
+  BsFillShareFill,
+} from "react-icons/bs";
+import {
+  TbDeviceWatchStats,
+  TbMessageCircle,
+  TbDiscount2,
+  TbTruckDelivery,
+} from "react-icons/tb";
+import { IoGameControllerOutline, IoRadioOutline } from "react-icons/io5";
+import { ImLocation } from "react-icons/im";
+import {
+  AiFillInstagram,
+  AiOutlineAppstore,
+  AiOutlineArrowLeft,
+  AiOutlineArrowRight,
+  AiOutlineUnorderedList,
+} from "react-icons/ai";
+import { RiCustomerService2Fill } from "react-icons/ri";
+import { SiAuth0 } from "react-icons/si";
+import { FcApproval } from "react-icons/fc";
 
-
-
- const  allIcons = {
-  BiShoppingBag:BiShoppingBag,
+const allIcons = {
+  BiShoppingBag: BiShoppingBag,
   FaRegUser: FaRegUser,
-   MdClose:MdClose,
-  FaMobileAlt:FaMobileAlt,
-  BsLaptop:BsLaptop,
-  FaTabletAlt:FaTabletAlt,
-  TbDeviceWatchStats:TbDeviceWatchStats,
-  MdCable:MdCable,
-  IoGameControllerOutline:IoGameControllerOutline,
-  IoRadioOutline:IoRadioOutline,
-  BiHeadphone:BiHeadphone,
-  TbMessageCircle:TbMessageCircle,
-  BsTelephoneFill:BsTelephoneFill,
-  MdEmail:MdEmail,
-  MdWatchLater:MdWatchLater,
+  MdClose: MdClose,
+  FaMobileAlt: FaMobileAlt,
+  BsLaptop: BsLaptop,
+  FaTabletAlt: FaTabletAlt,
+  TbDeviceWatchStats: TbDeviceWatchStats,
+  MdCable: MdCable,
+  IoGameControllerOutline: IoGameControllerOutline,
+  IoRadioOutline: IoRadioOutline,
+  BiHeadphone: BiHeadphone,
+  TbMessageCircle: TbMessageCircle,
+  BsTelephoneFill: BsTelephoneFill,
+  MdEmail: MdEmail,
+  MdWatchLater: MdWatchLater,
   ImLocation: ImLocation,
-  AiFillInstagram:AiFillInstagram,
-  BsTwitter:BsTwitter,
-  FaLinkedin:FaLinkedin,
-  FaFacebookF:FaFacebookF,
-  BsPinterest:BsPinterest,
-  TbDiscount2:TbDiscount2,
-  TbTruckDelivery:TbTruckDelivery,
-  RiCustomerService2Fill:RiCustomerService2Fill,
-  SiAuth0:SiAuth0,
+  AiFillInstagram: AiFillInstagram,
+  BsTwitter: BsTwitter,
+  FaLinkedin: FaLinkedin,
+  FaFacebookF: FaFacebookF,
+  BsPinterest: BsPinterest,
+  TbDiscount2: TbDiscount2,
+  TbTruckDelivery: TbTruckDelivery,
+  RiCustomerService2Fill: RiCustomerService2Fill,
+  SiAuth0: SiAuth0,
   FcApproval: FcApproval,
-  AiOutlineAppstore:AiOutlineAppstore,
-  FaRegEye:FaRegEye,
-  FaRegEyeSlash:FaRegEyeSlash ,
-  AiOutlineArrowLeft:AiOutlineArrowLeft,
-  AiOutlineArrowRight:AiOutlineArrowRight,
-  MdChevronRight:MdChevronRight,
-  MdChevronLeft:MdChevronLeft,
+  AiOutlineAppstore: AiOutlineAppstore,
+  FaRegEye: FaRegEye,
+  FaRegEyeSlash: FaRegEyeSlash,
+  AiOutlineArrowLeft: AiOutlineArrowLeft,
+  AiOutlineArrowRight: AiOutlineArrowRight,
+  MdChevronRight: MdChevronRight,
+  MdChevronLeft: MdChevronLeft,
+  MdFavoriteBorder: MdFavoriteBorder,
+  BsFillShareFill: BsFillShareFill,
+  MdOutlineNotificationsActive: MdOutlineNotificationsActive,
+  MdCompare:MdCompare,
+  AiOutlineUnorderedList:AiOutlineUnorderedList,
+} as const;
 
-  } as const
-
-  type allIcons=typeof allIcons
- export default allIcons
-
+type allIcons = typeof allIcons;
+export default allIcons;
