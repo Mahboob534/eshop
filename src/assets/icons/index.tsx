9359eb44f723936338b51b9129ab790d8256a530
atiyeh <mahboob6103@gmail.com>
2022-12-25 20:32:17 +0330

    fix:add new icon for produt page

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -9,50 +9,82 @@ import {
   FaRegEyeSlash,
 } from "react-icons/fa";
 
-import {MdClose ,MdCable,MdEmail,MdWatchLater,MdChevronRight,MdChevronLeft} from 'react-icons/md'
-import {BsLaptop,BsTelephoneFill,BsTwitter,BsPinterest} from 'react-icons/bs'
-import {TbDeviceWatchStats,TbMessageCircle,TbDiscount2,TbTruckDelivery} from 'react-icons/tb'
-import {IoGameControllerOutline,IoRadioOutline} from 'react-icons/io5'
-import {ImLocation} from 'react-icons/im'
-import {AiFillInstagram , AiOutlineAppstore,AiOutlineArrowLeft,AiOutlineArrowRight} from 'react-icons/ai'
-import {RiCustomerService2Fill} from 'react-icons/ri'
-import {SiAuth0} from 'react-icons/si'
-import {FcApproval} from 'react-icons/fc'
+import {
+  MdClose,
+  MdCable,
+  MdEmail,
+  MdWatchLater,
+  MdChevronRight,
+  MdChevronLeft,
+  MdFavoriteBorder,
+  MdOutlineNotificationsActive,
+  MdOutlineCompare,
+} from "react-icons/md";
+import {
+  BsLaptop,
+  BsTelephoneFill,
+  BsTwitter,
+  BsPinterest,
+} from "react-icons/bs";
+import {
+  TbDeviceWatchStats,
+  TbMessageCircle,
+  TbDiscount2,
+  TbTruckDelivery,
+} from "react-icons/tb";
+import { IoGameControllerOutline, IoRadioOutline } from "react-icons/io5";
+import { ImLocation } from "react-icons/im";
+import {
+  AiFillInstagram,
+  AiOutlineAppstore,
+  AiOutlineArrowLeft,
+  AiOutlineArrowRight,
+  AiOutlineUnorderedList
+} from "react-icons/ai";
+import { RiCustomerService2Fill } from "react-icons/ri";
+import { SiAuth0 } from "react-icons/si";
+import { FcApproval } from "react-icons/fc";
+import {BiLineChart} from 'react-icons/bi'
 
 const allIcons = {
-  BiShoppingBag: BiShoppingBag,
-  FaRegUser: FaRegUser,
-  MdClose: MdClose,
-  FaMobileAlt: FaMobileAlt,
-  BsLaptop: BsLaptop,
-  FaTabletAlt: FaTabletAlt,
-  TbDeviceWatchStats: TbDeviceWatchStats,
-  MdCable: MdCable,
-  IoGameControllerOutline: IoGameControllerOutline,
-  IoRadioOutline: IoRadioOutline,
-  BiHeadphone: BiHeadphone,
-  TbMessageCircle: TbMessageCircle,
-  BsTelephoneFill: BsTelephoneFill,
-  MdEmail: MdEmail,
-  MdWatchLater: MdWatchLater,
-  ImLocation: ImLocation,
-  AiFillInstagram: AiFillInstagram,
-  BsTwitter: BsTwitter,
-  FaLinkedin: FaLinkedin,
-  FaFacebookF: FaFacebookF,
-  BsPinterest: BsPinterest,
-  TbDiscount2: TbDiscount2,
-  TbTruckDelivery: TbTruckDelivery,
-  RiCustomerService2Fill: RiCustomerService2Fill,
-  SiAuth0: SiAuth0,
-  FcApproval: FcApproval,
-  AiOutlineAppstore:AiOutlineAppstore,
-  FaRegEye:FaRegEye,
-  FaRegEyeSlash:FaRegEyeSlash ,
-  AiOutlineArrowLeft:AiOutlineArrowLeft,
-  AiOutlineArrowRight:AiOutlineArrowRight,
-  MdChevronRight:MdChevronRight,
-  MdChevronLeft:MdChevronLeft,
+  BiShoppingBag,
+  FaRegUser,
+  MdClose,
+  FaMobileAlt,
+  BsLaptop,
+  FaTabletAlt,
+  TbDeviceWatchStats,
+  MdCable,
+  IoGameControllerOutline,
+  IoRadioOutline,
+  BiHeadphone,
+  TbMessageCircle,
+  BsTelephoneFill,
+  MdEmail,
+  MdWatchLater,
+  ImLocation,
+  AiFillInstagram,
+  BsTwitter,
+  FaLinkedin,
+  FaFacebookF,
+  BsPinterest,
+  TbDiscount2,
+  TbTruckDelivery,
+  RiCustomerService2Fill,
+  SiAuth0,
+  FcApproval,
+  AiOutlineAppstore,
+  FaRegEye,
+  FaRegEyeSlash,
+  AiOutlineArrowLeft,
+  AiOutlineArrowRight,
+  MdChevronRight,
+  MdChevronLeft,
+  MdFavoriteBorder,
+  MdOutlineNotificationsActive,
+  BiLineChart,
+  MdOutlineCompare,
+  AiOutlineUnorderedList
 
 } as const;
 export default allIcons;
